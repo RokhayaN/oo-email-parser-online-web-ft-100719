a259251437c7d1c083971971fcab3e86f34b0a95
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-23 05:33:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,5 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  def initialize (parse)
-    @parse=
+  attr_accessor :parse
+  
+  email_addresses = 
+    
